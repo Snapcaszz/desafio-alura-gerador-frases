16c47c09239d03ec873b753c83af1dedda59ea4a
Snapcaszz <matheus.scep@gmail.com>
2024-07-30 17:25:06 -0300

    spring starter project

---
+++ src/main/java/br/com/alura/geradorFrases/GeradorFrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.geradorFrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorFrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/geradorFrases/GeradorFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.geradorFrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
